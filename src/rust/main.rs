ef43f2ee396dac5fec7a47edf6b3b74a41a79439
kgaft <your_email@example.com>
2026-03-06 16:34:42 +0300

    working on ui

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -1,8 +1,12 @@
+#![feature(str_as_str)]
+
 slint::include_modules!();
 
 
 mod verbose;
 mod ui;
+pub mod assets;
+
 #[tokio::main]
 async fn main()  -> Result<(), slint::PlatformError>{
     ui::main_window::ui_window()
--- src/rust/ui/main_window.rs
+++ src/rust/ui/main_window.rs
@@ -1,10 +1,16 @@
 use slint::{ComponentHandle, SharedString};
-use crate::{MainWindow, PlayerComponentSettings};
+use crate::{MainWindow, PlayerComponentSettings, TrackInfo};
+use crate::assets::Assets;
 
-pub fn ui_window()  -> Result<(), slint::PlatformError> {
+pub fn ui_window() -> Result<(), slint::PlatformError> {
     let ui = MainWindow::new()?;
     ui.set_player_component_settings(PlayerComponentSettings{
-        title: SharedString::from("Player component")
+        track_info: TrackInfo{
+            album: SharedString::from("album"),
+            artist: SharedString::from("artist"),
+            picture:  slint::Image::load_from_svg_data(Assets::get("track.svg").unwrap().data.as_slice()).unwrap(),
+            title: SharedString::from("title"),
+        }
     });
     ui.run()
 }
